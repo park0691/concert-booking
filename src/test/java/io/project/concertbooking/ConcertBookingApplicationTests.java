e57a4cd59cc7203b9eedd4076695699a4e82605a
depark <mem29238@gmail.com>
2025-01-02 21:52:02 +0900

    first commit

---
+++ src/test/java/io/project/concertbooking/ConcertBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.concertbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
