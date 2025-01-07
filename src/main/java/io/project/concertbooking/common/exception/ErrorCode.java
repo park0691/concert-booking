16ce5278b637f0b11de97c4918abe382b1c5fc99
depark <mem29238@gmail.com>
2025-01-07 23:22:52 +0900

    feat: 비즈니스 예외 처리 핸들러 작성

---
+++ src/main/java/io/project/concertbooking/common/exception/ErrorCode.java
@@ -0,0 +1,24 @@
+package io.project.concertbooking.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    /* 400 BAD_REQUEST : 잘못된 요청 */
+
+    /* 401 UNAUTHORIZED : 인증되지 않은 요청 */
+
+    /* 404 NOT_FOUND : Resource를 찾을 수 없음 */
+    USER_NOT_FOUND(NOT_FOUND, "해당 유저의 정보를 찾을 수 없습니다.");
+
+    /* 409 CONFLICT : Resource의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
