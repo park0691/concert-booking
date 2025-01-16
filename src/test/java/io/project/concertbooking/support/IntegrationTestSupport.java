f267f61b69f496cec67ef43a6a181aa3214a4f14
depark <mem29238@gmail.com>
2025-01-16 13:40:58 +0900

    test: 테스트 격리를 위한 DB 초기화, 단위 테스트 공통 로직 클래스 작성

--- src/test/java/io/project/concertbooking/support/IntegrationTestSupport.java
+++ src/test/java/io/project/concertbooking/support/IntegrationTestSupport.java
@@ -2,6 +2,8 @@ package io.project.concertbooking.support;
 
 import com.navercorp.fixturemonkey.FixtureMonkey;
 import com.navercorp.fixturemonkey.api.introspector.BuilderArbitraryIntrospector;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -11,7 +13,15 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest
 public abstract class IntegrationTestSupport {
 
+    @Autowired
+    DBInitializer dbInitializer;
+
     protected FixtureMonkey fixtureMonkey = FixtureMonkey.builder()
             .objectIntrospector(BuilderArbitraryIntrospector.INSTANCE)
             .build();
+
+    @BeforeEach
+    void deleteAll() {
+        dbInitializer.deleteAll();
+    }
 }
